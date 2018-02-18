06b5961cf70e81872b06e942975551da00ad01d2
Anthony Nicholas <antman@Anthonys-MacBook-Pro.local>
2018-02-18 12:25:57 +1100

    Mockups

---
+++ moodActiveApp/moodActiveApp/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  moodActiveApp
+//
+//  Created by Anthony Nicholas on 17/2/18.
+//  Copyright © 2018 Anthony Nicholas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var mainCal: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
